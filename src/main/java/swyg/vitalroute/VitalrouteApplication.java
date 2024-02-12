b0627205842f2af0dfaf26db1426c4715e59c0e1
HwaJong-N <hwajong1016@gmail.com>
2024-02-12 13:21:42 +0900

    프로젝트 세팅

---
+++ src/main/java/swyg/vitalroute/VitalrouteApplication.java
@@ -0,0 +1,13 @@
+package swyg.vitalroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swyg/vitalroute/VitalrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package swyg.vitalroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
